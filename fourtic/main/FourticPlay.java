f5a4ee81b7288760d53d74b3f6ec06baea811e90
GoodGuyGregory <greg.witt625@gmail.com>
2023-11-06 00:21:36 -0500

    adds initial AI player and homework submission

---
+++ fourtic/main/FourticPlay.java
@@ -0,0 +1,15 @@
+public class FourticPlay {
+
+    public static printBoard(char[][] position) {
+
+    }
+
+    public static void main(String[] args) {
+
+        char[][] initialPosition = {
+                { { '.', '.', '.', '.' }, { '.', '.', '.', '.' }, { '.', '.', '.', '.' }, { '.', '.', '.', '.' }, } };
+
+        System.out.println("Welcome to Fourtic ");
+
+    }
+}
